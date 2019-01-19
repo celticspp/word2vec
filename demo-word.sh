673ae6843f7227b5a769b9bc355d2242a078ca9c
QiruiLi <qiruili@QiruiLi.local>
2019-01-19 23:24:59 +0800

    add

--- demo-word.sh
+++ demo-word.sh
@@ -1,7 +1,11 @@
 make
 if [ ! -e text8 ]; then
-  wget http://mattmahoney.net/dc/text8.zip -O text8.gz
-  gzip -d text8.gz -f
+    if [ ! -e text8.gz ];then
+       # wget http://mattmahoney.net/dc/text8.zip -O text8.gz
+       curl -o text8.gz http://mattmahoney.net/dc/text8.zip
+    fi
+       # gzip -d text8.gz -f
+     tar -zxvf text8.gz
 fi
 time ./word2vec -train text8 -output vectors.bin -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
 ./distance vectors.bin
